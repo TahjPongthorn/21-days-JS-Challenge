4cfbfc023e38fa1c164b3f4acca62a06cb83e490
Pongthorn Chumpoo <chumpoo.dev@gmail.com>
2021-05-03 22:16:41 +0700

    Added snow effect

--- CountDownClock/snow-script.js
+++ CountDownClock/snow-script.js
@@ -1,33 +1,5 @@
 (() => {
-	const second = 1000;
-	const minute = second * 60;
-	const hour = minute * 60;
-	const day = hour * 24;
-
-	function setTimeToHtmlElement(idName, text) {
-		const element = document.getElementById(idName);
-		element.innerText = text;
-	}
-
-	function countDown() {
-		const now = new Date().getTime();
-		const christmas = new Date("December 25, 2021 23:59:59").getTime();
-		const timeLeft = christmas - now;
-
-		setTimeToHtmlElement("days", Math.floor(timeLeft / day));
-		setTimeToHtmlElement("hours", Math.floor((timeLeft % day) / hour));
-		setTimeToHtmlElement("minutes", Math.floor((timeLeft % hour) / minute));
-		setTimeToHtmlElement("seconds", Math.floor((timeLeft % minute) / second));
-	}
-	function run() {
-		countDown();
-		setInterval(countDown, second);
-		snowEffect();
-	}
-	run();
-})();
-
-function snowEffect() {
+	// เริ่มเขียนโค้ด
 	function setup() {
 		const canvas = document.getElementById("falling-snow-canvas");
 		canvas.width = window.innerWidth;
@@ -84,4 +56,4 @@ function snowEffect() {
 		}, 50);
 	}
 	run();
-}
+})();
